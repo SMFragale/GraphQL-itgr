38a2106b17e598d3503c3e0910f6af1ff58c1f2d
Sebastian Molano <smolanof@javeriana.edu.co>
2022-11-18 19:02:27 -0500

    Initial commit

---
+++ src/test/java/mainTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class mainTest {
+
+    @Test
+    public void testService() {
+        try {
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
